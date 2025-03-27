0d28689ad0e530321b0ef3e37d9b923e0ee8f626
ilay030106 <ilay8073@gmail.com>
2025-03-27 18:42:44 +0200

    initial commit

--- IAF_Project/src/textBlocks.jsx
+++ IAF_Project/src/textBlocks.jsx
@@ -5,24 +5,31 @@ function Blocks() {
   return (
     <>
       <div className="card">
-        <h2 id="altitude" className="dataTitle">
+        <h2 id="altitude" className="data-title">
           {altitude}
         </h2>
-        <h3 id="altitudeVal">PlaceHolder</h3>
+        <h3 id="altitudeVal" className="data-info">
+          PlaceHolder
+        </h3>
       </div>
       <br />
       <div className="card">
-        <h2 id="HIS" className="dataTitle">
+        <h2 id="HIS" className="data-title">
           {HIS}
         </h2>
-        <h3 id="HIS_Val">PlaceHolder</h3>
+
+        <h3 id="HIS_Val" className="data-info">
+          PlaceHolder
+        </h3>
       </div>
       <br />
       <div className="card">
-        <h2 id="ADI" className="dataTitle">
+        <h2 id="ADI" className="data-title">
           {ADI}
         </h2>
-        <h3 id="ADI_Val">PlaceHolder</h3>
+        <h3 id="ADI_Val" className="data-info">
+          PlaceHolder
+        </h3>
       </div>
       <br />
     </>
